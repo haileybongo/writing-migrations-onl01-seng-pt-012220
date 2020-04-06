b9244524a61317d2d2fca2000dfcfd1c0786bc7d
Hailey Bongo <haileyebongo@gmail.com>
2020-04-06 02:14:40 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,6 +1,5 @@
-class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.2]
   def change 
-    add_column :students, :grade, :integer
-    add_column :students, :birthdate, :string 
+    change_column :students, :birthdate, :datetime 
   end
 end
